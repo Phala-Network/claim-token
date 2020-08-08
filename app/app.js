e5fbdecba110a7549d3ac0d122c83a0a27151ded
goldenfiredo <goldenfiredo@gmail.com>
2020-08-08 09:56:01 +0800

    add nodejs main script

---
+++ app/app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const express = require("express");  
+const app = express();  
+app.use(express.static(__dirname + "/public"))
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+var httpServer = http.createServer(app);
+httpServer.listen(8088)
+
